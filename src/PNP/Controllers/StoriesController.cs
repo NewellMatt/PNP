70f7155d83332471aa6e3f47ef24b8fc70e8faa6
mattnewell <mnewell10@gmail.com>
2016-05-11 16:36:35 -0700

    Add ability to edit a story

--- src/PNP/Controllers/StoriesController.cs
+++ src/PNP/Controllers/StoriesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using PNP.Models;
+using Microsoft.Data.Entity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,11 +21,22 @@ namespace PNP.Controllers
         {
             return View();
         }
-
         [HttpPost]
         public IActionResult Create(Story item)
         {
             db.Stories.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisStory = db.Stories.FirstOrDefault(items => items.StoryId == id);
+            return View(thisStory);
+        }
+        [HttpPost]
+        public IActionResult Edit(Story story)
+        {
+            db.Entry(story).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }

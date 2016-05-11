0df29b02a7c2e82aa988160e0ad4782f0465b1cd
mattnewell <mnewell10@gmail.com>
2016-05-11 15:02:46 -0700

    Add HomeController and Home index to return first view

--- src/PNP/Startup.cs
+++ src/PNP/Startup.cs
@@ -15,12 +15,20 @@ namespace PNP
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {

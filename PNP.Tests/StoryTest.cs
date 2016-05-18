9195613b4204026adf9191c2008446eb4cec2c15
mattnewell <mnewell10@gmail.com>
2016-05-17 18:57:20 -0700

    Add testing

---
+++ PNP.Tests/StoryTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using PNP.Models;
+
+namespace PNP.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class StoryTest
+    {
+        [Fact]
+        public void GetStoryDescriptionTest()
+        {
+            //Arrange
+            var story = new Story();
+            story.Content = "Great story";
+            //Act
+            var result = story.Content;
+
+            //Assert
+            Assert.Equal("Great story", result);
+        }
+    }
+}
